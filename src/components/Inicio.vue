a5b210d8fe6cd8f4d652d217860fd64f2f7d5daf
Ruben Mariscal <92536400+rmargil133@users.noreply.github.com>
2023-02-17 12:10:49 +0100

    Add files via upload

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,12 +1,20 @@
 <template>
-        <main class="home">
-        <img class="home__imagen" src="../assets/d.png" alt="fondo-inicio">
+  <main class="home">
+    <img class="home__imagen" src="../assets/d.png" alt="fondo-inicio">
+    
+    <div class="home_container">
+      <h2 class="home__titulo">SOBRE MI TRABAJO</h2>
 
-          <input placeholder="Buscar Articulo" type="text" class="home__buscador" name="articulo"/>
-          <button type="submit" class="home__boton">Buscar!</button>  
+      <p>Est. 2015 surge como alternativa ocupacional para tener la mente activa fuera de horarios laborales y problemas del dia dia a los que muchos estamos sometidos sin ni siquiera saberlo, surge la idea de poder darle una segunda oportunidad a todos esos cacharros arrumbados en el taller que me vio crecer y donde tantas historias he vivido….</p>
 
+      <p>eruhogeqriuhgiueqrhgiurhtgiquh</p>  
 
-      </main>
+    <button class="home__readMore_btn">Read More</button>
+
+  </div>
+    <input placeholder="Buscar Articulo" type="text" class="home__buscador" name="articulo"/>
+    <button type="submit" class="home__boton">Buscar!</button>  
+  </main>
 </template>
 
 <script>
@@ -14,3 +22,7 @@ export default {
   name: 'InicioVue'
 }
 </script>
+
+<script>
+</script>
+
